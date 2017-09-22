a2884a4965208622ef91c2c9ed6c42430b897e1b
Helmi Satria <satriahelmi@gmail.com>
2017-09-23 06:38:48 +0700

    last commit

--- tupro1.js
+++ tupro1.js
@@ -1,63 +1,56 @@
-let randomOneNumber = (max, min) => {
-  return Math.random() * (max-min) + min
-}
+console.time('Running Time');
 
-let hitungCost = (x1, x2) => {
-  return (
-    ( 4 - (2.1 * Math.pow(x1, 2)) + (Math.pow(x1, 4)/3) ) * Math.pow(x1, 2) + x1*x2 + (-4 + (4 * Math.pow(x2, 2))) * Math.pow(x2, 2)
+let randomOneNumber = (max, min) => Math.random() * (max-min) + min
+
+let hitungCost = (x1, x2) => (
+    ( 4 - (2.1 * Math.pow(x1, 2)) + (Math.pow(x1, 4)/3) )
+    * Math.pow(x1, 2) + x1*x2 + (-4 + (4 * Math.pow(x2, 2))) * Math.pow(x2, 2)
   )
-}
 
-let randomNeighbor = (max, min) => {
-  return (Math.random() * (max - min )) + min
-}
+let randomNeighbor = (max, min) => (Math.random() * (max - min )) + min
 
-let acceptanceProbability = (oldcost, newcost, t) => {
-  return Math.exp((oldcost - newcost)/t)
-}
+let acceptanceProbability = (cost, newcost, t) => Math.exp((cost - newcost)/t)
 
-let initx1 = randomOneNumber(10, -10)
-let initx2 = randomOneNumber(10, -10)
 let x1 = randomOneNumber(10, -10)
 let x2 = randomOneNumber(10, -10)
 
 let newx1, newx2, newcost
 
-let oldcost = hitungCost(x1, x2)
+let cost = hitungCost(x1, x2)
 let TempMax = 1.0
-let TempMin = 0.0000001
-let alpha = 0.8
-let loopCount = 100
+let TempMin = 0.000001
+let alpha = 0.99
+let loopCount = 1000 //makin besar iterasi makin optimal pencarian nilai minimumnya
 while (TempMax > TempMin) {
   let i = 1
   while (i <= loopCount) {
     newx1 = x1 + randomNeighbor(10, -10)
     newx2 = x2 + randomNeighbor(10, -10)
     newcost = hitungCost(newx1, newx2)
-    let ap = acceptanceProbability(oldcost, newcost, TempMax)
+    let ap = acceptanceProbability(cost, newcost, TempMax)
     if (ap > Math.random()) {
       x1 = newx1
       x2 = newx2
-      oldcost = newcost
+      cost = newcost
     }
     i++
   }
   TempMax = TempMax * alpha
 }
+console.timeEnd('Running Time')
 
-// oldcost = -2
+// cost = -2
 let akurasi = () => {
   let FR = -1.03163
-  // let FR = 1.5
-  return (1 - ((oldcost - FR)/FR))* 100
+  // let FR = -0.99
+  return (1 - ((cost - FR)/FR))* 100
 }
 
 console.log(JSON.stringify({
-  hitungcost : hitungCost(2,2),
-  initx1, initx2,
+  cekHitunCost : hitungCost(2,2),
   x1, x2,
   newx1, newx2,
-  oldcost,
   newcost,
+  cost,
   akurasi: akurasi(),
 }, null, 2))
